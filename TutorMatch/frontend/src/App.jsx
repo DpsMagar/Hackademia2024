752978bc62c65033c9aad0c20a250435bb2210f4
DpsMagar <dipeshpaharai@gmail.com>
2024-09-21 02:53:27 +0545

    basic react done

--- TutorMatch/frontend/src/App.jsx
+++ TutorMatch/frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from '../Components/Layout';
-import Login from '../Components/Login';
-import HomePage from '../Components/HomePage';
-import About from '../Components/About';
-import Logout from '../Components/Logout';
-import MainPage from '../Components/MainPage';
-import Register from '../Components/Register';
-import Navbar from '../Components/Navbar';
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import UserHomepage from "./pages/user/homepage"
+import TutorHomepage from "./pages/tutor/homepage"
+import YourChoice from "./pages/user/YourChoice"
+import Courses from "./pages/tutor/Courses"
+import AddCourse from "./pages/tutor/AddCourse"
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Layout><Login/></Layout>} /> 
-                <Route path="/register" element={<Layout><Register/></Layout>} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/nav" element={<Navbar />} />
-                <Route path="/about" element={<Layout><About/></Layout>}/>
-                <Route path="/home" element={<Layout><MainPage/></Layout>}/> 
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        {/* tutor  */}
+        <Route path="/tutor/dashboard" element={<TutorHomepage />} />
+        <Route path="/tutor/courses" element={<Courses />} />
+        <Route path="/tutor/add-course" element={<AddCourse />} />
+
+        {/* user  */}
+        <Route path="/" element={<UserHomepage />} />
+        <Route path="/choose-you-like" element={<YourChoice />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
